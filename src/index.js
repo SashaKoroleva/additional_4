462ab73d64824b4c60b60938119ae5650f9469f7
SashaKoroleva <s.sashakoroleva.s@gmail.com>
2017-09-23 00:36:40 +0300

    Oh, my God! I did it!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,101 @@
+function sameLength(a,count) {
+    a = '0'.repeat(count) + a;
+    return a;
+}
+function multNumber(first, second) {
+
+  var str;
+  var mult = '';
+  var s = [];
+
+  for( var i = 1; i < second.length+1; i++){
+      var j = 1;
+      var save = 0;
+      while( j < second.length+1) {
+      str = Number(first.charAt(first.length-j))*Number(second.charAt(second.length-i))+save;
+        if(str > 9) {
+          str = String(str);
+          save = Number(str.charAt(0));
+          mult = str.charAt(1) + mult;
+        } else {
+          save = 0;
+          str = String(str);
+          mult = str + mult;
+        }
+        j++;
+        if(j==second.length+1){
+        mult = save + mult;
+      }
+      }
+
+      s[i-1] = String(mult) + '0'.repeat(i-1) ;
+      mult = '';
+    }
+
+    for(var i = s.length-1; i>0; i--){
+      var zero = '0'.repeat(i);
+      s[s.length - 1 - i] = zero + s[s.length - 1 - i];
+    }
+
+    return s;
+}
+function addition(s){
+
+  var str = 0;
+  var result = '';
+  var long = s[0].length;
+  var save =0;
+
+  for(var i=1; i<long+1; i++){
+
+  var j = 0;
+
+   while( j < s.length) {
+      str = Number(str) + Number(s[j].charAt(long-i));
+      j++;
+    }
+    str = str+save;
+    if(str > 9) {
+      str = String(str);
+      save = Number((str.substring(0, str.length - 1)));
+      result = str.charAt(str.length - 1) + result;
+      str ="";
+    }
+    else {
+      str = String(str);
+      save = 0;
+      result = str + result;
+      str="";
+    }
+    if(i==long && save != 0){
+          result = save + result;
+        }
+
+  }
+
+ return result;
+}
+
+
+
+
+
+
+
 module.exports = function multiply(first, second) {
-  // your solution
+
+  if(first.length < second.length) {
+    first = sameLength(first, second.length - first.length);
+  } else if(first.length > second.length) {
+      second = sameLength(second, first.length - second.length);
+  }
+
+  var sum = multNumber(first, second);
+
+  var result = addition(sum);
+
+  while(result.charAt(0) == 0){
+    result = result.slice(1);
+}
+return result;
 }
